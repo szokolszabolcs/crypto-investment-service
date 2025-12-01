2f3119675f4f1492272982933aebd3cf854031f2
Szabolcs Szokol <szabolcs_szokol@epam.com>
2025-12-01 12:57:39 +0100

    Initial commit

---
+++ src/main/java/com/xm/crypto/CryptoInvestmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xm.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInvestmentServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CryptoInvestmentServiceApplication.class, args);
+	}
+
+}
